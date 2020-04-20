0cdeea4dc62f04136691b751929ad7e158ff7334
Daniel Min <danielm710@gmail.com>
2020-04-20 02:29:30 -0400

    resolved warnings

--- frontend/src/redux/actions/textareaAction.js
+++ frontend/src/redux/actions/textareaAction.js
@@ -1,7 +1,7 @@
 import { TEXTAREA_HANDLE_CHANGE } from './types';
 
 export const textareaHandleChange = (e) => dispatch => {
-	const {name, value, files} = e.target
+	const { value } = e.target
 
 	dispatch({
 		type: TEXTAREA_HANDLE_CHANGE,
