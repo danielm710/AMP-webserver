7ce081afb9e80199d59ea52355b404de5e0ccd4b
Daniel Min <danielm710@gmail.com>
2020-04-20 02:13:16 -0400

    Task progress related code here

---
+++ frontend/src/Components/Progress/TaskProgress.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import io from "socket.io-client";
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { getLoadingStatus, getRedirectStatus } from '../../redux/actions/loadingAction'
+
+function TaskProgress(props) {
+	let isLoading;
+	let isDataDone;
+	const [data, setData] = useState('');
+
+	// Redux action
+	const { getLoadingStatus, getRedirectStatus } = props;
+
+	const endpoint = "http://localhost:5000"
+	
+	useEffect(() => {
+		const socket = io.connect(endpoint);
+
+		socket.on("error", err => {
+			console.log("SocketIO error")
+			console.log(err)
+		})
+
+		socket.on("connect", () => {console.log("SocketIO connected!")})
+
+		socket.on("test", (data) => {setData(data.data)})
+
+		// Clean up
+		return () => {
+			socket.off("test")
+		}
+	}, [])
+	
+	useEffect(() => {
+		isLoading = (data === 'Done!' || data === '') ? false : true
+		getLoadingStatus(isLoading)
+
+		isDataDone = (data === 'Done!') ? true : false
+		getRedirectStatus(isDataDone)
+
+		return () => {
+			console.log("data useEffect unmounting...")
+		}
+	}, [data])
+	
+	return (
+		<div>
+			<p style={isLoading ? {display: 'block'} : {display: 'None'}}>Loading...</p>
+			<p>Data is "{data}"</p>
+		</div>
+	)
+}
+
+const mapStateToProps = state => ({
+})
+
+const mapDispatchToProps = {
+	getLoadingStatus,
+	getRedirectStatus
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TaskProgress);
