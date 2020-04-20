e8b311f3621f2d40c329dc92ffd9ad2e99b1604c
Daniel Min <danielm710@gmail.com>
2020-04-20 02:17:33 -0400

    removed old codes

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Routes from './Routes'
-
-//ReactDOM.render(<Home />, document.getElementById('root'));
-ReactDOM.render(<Routes />, document.getElementById('root'));
--- src/redux/actions/types.js
+++
@@ -1,4 +0,0 @@
-export const GET_UUID = 'GET_UUID';
-export const REQUEST_LOAD = 'REQUEST_LOAD'
-export const REQUEST_SUCCESS = 'REQUEST_SUCCESS'
-export const REQUEST_FAIL = 'REQUEST_FAIL'
