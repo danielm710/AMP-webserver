9b90431c6b1cf6b76422e65de00c913cfdb56377
Daniel Min <danielm710@gmail.com>
2020-04-20 02:14:16 -0400

    minor change

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store'
 
@@ -9,11 +9,13 @@ import Result from './Containers/Result/Result'
 const Routes = () => (
 	<Provider store={store}>
 		<Router>
-			<Route exact path="/" component={Home} />
-			<Route 
-				path="/:result" 
-				component={Result} 
-			/>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route 
+					path="/:result" 
+					component={Result} 
+				/>
+			</Switch>
 		</Router>
 	</Provider>
 );
