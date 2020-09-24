c0cf3515b9691c3032c6788d045c4931bb660f01
Daniel Min <danielm710@gmail.com>
2020-09-23 17:47:39 -0700

    minor change

--- backend/server.js
+++ backend/server.js
@@ -23,15 +23,3 @@ app.use((req, res, next) => {
 
 // Routers (Needs this as the first route)
 app.use('/', apiRoute);
-
-// For production uses only
-//if(process.env.NODE_ENV === 'production') {
-//	console.log("------Production Mode------")
-//	//app.use('/static', express.static('/frontend/build/static'));
-//	app.use('/static', express.static('build/static'));
-//
-//	app.get('/*', (req, res)=> {  
-//		res.sendFile(path.join(__dirname, '/build/index.html'));
-//		//res.sendFile('./build/index.html');
-//	});
-//};
